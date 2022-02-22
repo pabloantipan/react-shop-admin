91e10b5fec5a741e1f0d97bcf6b6edf3636f4cfe
Pablo Antipan <antipan.pablo@gmail.com>
2022-02-21 23:17:58 -0300

    modeling login

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,21 +1,14 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
-const Loading = () => {
+export default function Nav() {
+  const router = useRouter();
+  const route = router.pathname.substring(1);
+
   return (
-    <div className="flex items-center justify-center w-full h-full pt-96">
-      <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
-        <svg fill="none" className="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <path
-            clipRule="evenodd"
-            d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
-            fill="currentColor"
-            fillRule="evenodd"
-          />
-        </svg>
-        <div>Loading ...</div>
+    <nav className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900 capitalize">{route}</h1>
       </div>
-    </div>
+    </nav>
   );
-};
-
-export default Loading;
+}
