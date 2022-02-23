6f692336eb3c93defffd839b5018f5e9191ddaac
Pablo Antipan <antipan.pablo@gmail.com>
2022-02-22 23:31:40 -0300

    adding auth custom hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext, createContext } from 'react';
+import Cookie from 'js-cookie';
+import axios from 'axios';
+
+const AuthContext = createContext();
+
+export function ProviderAuth({ children }) {
+  const auth = useProvideAuth();
+  return <AuthContext.Provider value={auth}>{}children</AuthContext.Provider>;
+}
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+function userProviderAuth() {
+  const [user, setUser] = useState(null);
+
+  const signIn = async (email, password) => {};
+
+  return {
+    user,
+    signIn,
+  };
+}
